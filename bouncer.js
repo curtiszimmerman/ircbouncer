a1ab855292523fb32e19d2669d1ada9cdd4ca7d6
curtisz <czimmerman@ezoic.com>
2014-07-28 00:22:19 -0700

    initial commit

---
+++ bouncer.js
@@ -0,0 +1,27 @@
+/**
+ * @project ircbouncer
+ * IRC Bouncer
+ * @file bouncer.js
+ * Primary application driver
+ * @author curtis zimmerman
+ * @contact software@curtisz.com
+ * @license GPLv2
+ * @version 0.0.1a
+ */
+
+/**
+ * @cite http://http://stackoverflow.com/questions/7137397/module-exports-vs-exports-in-nodejs
+ */
+module.exports = exports = __bouncer = (function() {
+	/**
+	 * @cite http://www.nczonline.net/blog/2012/03/13/its-time-to-start-using-javascript-strict-mode/
+	 */
+	"use strict";
+
+	var fs = require('fs');
+	var http = require('http');
+	var net = require('net');
+	var url = require('url');
+
+	
+})();
