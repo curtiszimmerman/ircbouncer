87c09b3300389b81813a696520824614c9fe8a55
curtisz <czimmerman@ezoic.com>
2014-07-28 00:59:45 -0700

    added: pubsub function object, primary network client/server stubs

--- bouncer.js
+++ bouncer.js
@@ -50,4 +50,84 @@ module.exports = exports = __bouncer = (function() {
 			}
 		}
 	};
+
+	var _log = (function() {
+		var _con = function( data, type ) {
+			var pre = ['[i] DEBUG: ','[!] ERROR: ','[+] '];
+			console.log(pre[type]+data);
+		};
+		var _dbg = function( data ) {
+			if (__data.state.debug) _con(data, 0);
+		};
+		var _err = function( data ) {
+			_con(data, 1);
+		};
+		var _log = function( data ) {
+			_con(data, 2);
+		};
+		return {
+			dbg: _dbg,
+			err: _err,
+			log: _log
+		}
+	})();
+
+	var _pubsub = (function() {
+		var _cache = {};
+		var _flush = function() {
+			cache = {};
+		};
+		var _pub = function( topic, args, scope ) {
+			if (cache[topic]) {
+				var currentTopic = cache[topic],
+					topicLength = currentTopic.length;
+				for (var i=0; i<topicLength; i++) {
+					currentTopic[i].apply(scope || this, args || []);
+				}
+			}
+		};
+		var _sub = function( topic, callback ) {
+			if (!cache[topic]) {
+				cache[topic] = [];
+			}
+			cache[topic].push(callback);
+			return [topic, callback];
+		};
+		var _unsub = function( handle, total ) {
+			var topic = handle[0],
+				cacheLength = cache[topic].length;
+			if (cache[topic]) {
+				for (var i=0; i<cacheLength; i++) {
+					if (cache[topic][i] === handle) {
+						cache[topic].splice(cache[topic][i], 1);
+						if (total) {
+							delete cache[topic];
+						}
+					}
+				}
+			}
+		};
+		return {
+			flush: _flush,
+			pub: _pub,
+			sub: _sub,
+			unsub: _unsub
+		}
+	})();
+
+	var init = (function() {
+		_log.log('bouncer initialized!');
+	})();
+
+	var clientIRC = (function() {
+		// handle outbound IRC connection (e.g. connection to freenode)
+	})();
+
+	var serverHTTP = (function() {
+		// handle inbound HTTP stat requests
+	})();
+
+	var serverIRC = (function() {
+		// handle inbound IRC connection
+	})();
 })();
